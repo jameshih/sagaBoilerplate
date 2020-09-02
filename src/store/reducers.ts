df3264f281e0113001fc714a4d10f5c3c5af79c0
jameshih <j@shih.app>
2020-09-02 17:00:33 -0400

    added types

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./user/userReducer";
 
-export type RootState = ReturnType<typeof rootReducer>;
 export const rootReducer = combineReducers({
   user: userReducer,
 });
